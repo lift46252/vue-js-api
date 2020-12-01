8d5e17456014bd03c14a873f02ece30672a66ef6
Lefter Alex <sasa.lefter@gmail.com>
2020-12-01 21:25:39 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,86 @@
+<template>
+<div>
+    <v-app-bar
+      color="black"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>My Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-filter</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="blue--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{ name: 'home' }" class="link">
+                Home
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>...</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>About Us</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Contact Us</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>    
+</div>
+
+</template>
+
+<script>
+    export default {
+        name:"Navbar",
+        data: () => ({
+        drawer: false,
+        group: null,
+        }),
+
+        watch: {
+        group () {
+            this.drawer = false
+        },
+        },
+    }
+</script>
+
+<style scoped>
+.link{
+    text-decoration: none;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import store from "./store"
+import VueRouter from 'vue-router'
+import Home from './components/Home'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: "/", component: Home, name: "home"},
+];
+
+const router = new VueRouter({
+  routes
+});
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
